b9e425b086c57fbbd5b4b35a8235d406c8502c0d
Stewar Marin <stewar1020@gmail.com>
2021-09-02 19:21:59 -0500

    Finish WIP - Added consolidate schedule

--- watchStewar/watchStewar.Functions/Functions/ScheduleFunction.cs
+++ watchStewar/watchStewar.Functions/Functions/ScheduleFunction.cs
@@ -2,6 +2,8 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using Microsoft.WindowsAzure.Storage.Table;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using watchStewar.Functions.Entities;
 
@@ -19,23 +21,76 @@ namespace watchStewar.Functions.Functions
 
             string filter = TableQuery.GenerateFilterConditionForBool("isConsolidate", QueryComparisons.Equal, false);
             TableQuery<WatchEntity> query = new TableQuery<WatchEntity>().Where(filter);
-            TableQuerySegment<WatchEntity> consolidatesRegisters = await watchTable.ExecuteQuerySegmentedAsync(query, null);
-            int consolidateData = 0;
+            TableQuerySegment<WatchEntity> unconsolidatesRegisters = await watchTable.ExecuteQuerySegmentedAsync(query, null);
+            int totalAdded = 0;
+            int totalUpdated = 0;
 
-            foreach (WatchEntity consolidateRegister in consolidatesRegisters)
+            //Set by id
+            List<IGrouping<int, WatchEntity>> groupUnconsolidate = unconsolidatesRegisters.GroupBy(u => u.idWorker).OrderBy(u => u.Key).ToList();
+            foreach (IGrouping<int, WatchEntity> group in groupUnconsolidate)
             {
-                consolidateRegister.isConsolidate = true;
-                await watchTable.ExecuteAsync(TableOperation.Replace(consolidateRegister));
-                ConsolidateEntity consolidateEntity = new ConsolidateEntity
+                TimeSpan difference;
+                double totalMinutes = 0;
+                List<WatchEntity> orderedRegisters = group.OrderBy(g => g.register).ToList();
+                int isEven = orderedRegisters.Count % 2 == 0 ? orderedRegisters.Count : orderedRegisters.Count - 1;
+                WatchEntity[] watchesAuxiliar = orderedRegisters.ToArray();
+                try
                 {
-                    idWorker = consolidateRegister.idWorker,
-                    date = DateTime.UtcNow,
-                    minutesWorked = 100
-                };
-                await consolidateTable.ExecuteAsync(TableOperation.Insert(consolidateEntity));
-                consolidateData++;
+                    for (int i = 0; i < isEven; i++)
+                    {
+                        await ChangeConsolidateStatus(watchesAuxiliar[i].RowKey, watchTable);
+                        if (i % 2 != 0 && watchesAuxiliar.Length > 1)
+                        {
+                            difference = watchesAuxiliar[i].register - watchesAuxiliar[i - 1].register;
+                            totalMinutes += difference.TotalMinutes;
+                            TableQuerySegment<ConsolidateEntity> allConsolidated = await consolidateTable.ExecuteQuerySegmentedAsync(new TableQuery<ConsolidateEntity>(), null);
+                            IEnumerable<ConsolidateEntity> existConsolidated = allConsolidated.Where(x => x.idWorker == watchesAuxiliar[i].idWorker);
+                            if (existConsolidated == null || existConsolidated.Count() == 0)
+                            {
+                                ConsolidateEntity consolidated = new ConsolidateEntity
+                                {
+                                    idWorker = watchesAuxiliar[i].idWorker,
+                                    date = DateTime.Today,
+                                    minutesWorked = (int)totalMinutes,
+                                    ETag = "*",
+                                    PartitionKey = "ConsolidatedRegisters",
+                                    RowKey = watchesAuxiliar[i].RowKey
+                                };
+                                TableOperation addConsolidatedOperation = TableOperation.Insert(consolidated);
+                                await consolidateTable.ExecuteAsync(addConsolidatedOperation);
+                                totalAdded++;
+                            }
+                            else
+                            {
+                                TableOperation findOp = TableOperation.Retrieve<ConsolidateEntity>("ConsolidatedRegisters", existConsolidated.First().RowKey);
+                                TableResult findRes = await consolidateTable.ExecuteAsync(findOp);
+                                ConsolidateEntity consolidatedEntity = (ConsolidateEntity)findRes.Result;
+                                consolidatedEntity.minutesWorked += existConsolidated.First().minutesWorked;
+                                TableOperation addConsolidatedOperation = TableOperation.Replace(consolidatedEntity);
+                                await consolidateTable.ExecuteAsync(addConsolidatedOperation);
+                                totalUpdated++;
+                            }
+                        }
+                    }
+                }
+                catch (Exception error)
+                {
+                    string errorMessage = error.Message;
+                    throw;
+                }
             }
-            log.LogInformation($"{consolidateData} was counted");
+            log.LogInformation($"Well done!. \n  New data saved was: {totalAdded} \n Data updated was: {totalUpdated}.");
+
+        }
+
+        private static async Task ChangeConsolidateStatus(string id, CloudTable watchTable)
+        {
+            TableOperation findOperation = TableOperation.Retrieve<WatchEntity>("RegisterTable", id);
+            TableResult findResult = await watchTable.ExecuteAsync(findOperation);
+            WatchEntity timeEntity = (WatchEntity)findResult.Result;
+            timeEntity.isConsolidate = true;
+            TableOperation addOperation = TableOperation.Replace(timeEntity);
+            await watchTable.ExecuteAsync(addOperation);
         }
     }
 }
