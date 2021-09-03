0a0c5543cfb7e6066670b04d9f03c74c52288418
Stewar Marin <stewar1020@gmail.com>
2021-09-03 10:08:47 -0500

    Added schedule test function

---
+++ watchStewar/watchStewar.Tests/Test/ScheduleFunctionTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using watchStewar.Functions.Functions;
+using watchStewar.Tests.Helpers;
+using Xunit;
+
+namespace watchStewar.Tests.Test
+{
+    public class ScheduleFunctionTest
+    {
+        [Fact]
+        public void ScheduleFunction_Should_Log_Message()
+        {
+
+            //arrange
+            ListLogger logger = (ListLogger)TestFactory.CreateLogger(LoggerTypes.List);
+            MockCloudTableWatches watchTable = new MockCloudTableWatches(new Uri("http://127.0.0.1:10002/devstoreaccount1/reports"));
+            MockCloudTableWatches consolidateTable = new MockCloudTableWatches(new Uri("http://127.0.0.1:10002/devstoreaccount1/reports"));
+
+
+            //act
+            ScheduleFunction.Run(null, watchTable, consolidateTable, logger);
+            string message = logger.Logs[0];
+
+            //assert
+            Assert.Contains("Consolidating data in table consolidate", message);
+        }
+    }
+}
