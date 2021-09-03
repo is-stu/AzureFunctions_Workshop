6a689f9bdd8950f1694a620e058a78925ee50e70
Stewar Marin <stewar1020@gmail.com>
2021-09-02 23:37:25 -0500

    Added first couple tests create & update

--- watchStewar/watchStewar.Tests/Helpers/ListLogger.cs
+++ watchStewar/watchStewar.Tests/Helpers/ListLogger.cs
@@ -1,11 +1,32 @@
 ﻿using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace watchStewar.Tests.Helpers
 {
     public class ListLogger : ILogger
     {
+        public ListLogger()
+        {
+            Logs = new List<string>();
+        }
+
+        public IList<string> Logs;
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return NullScope.Instance;
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return false;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            string message = formatter(state, exception);
+            Logs.Add(message);
+        }
     }
 }
---
+++ watchStewar/watchStewar.Tests/Test/WatchAPITest.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using watchStewar.Common.Models;
+using watchStewar.Functions.Functions;
+using watchStewar.Tests.Helpers;
+using Xunit;
+
+namespace watchStewar.Tests.Test
+{
+    public class WatchAPITest
+    {
+        private readonly ILogger logger = TestFactory.CreateLogger();
+
+        [Fact]
+        public async void CreateWatch_Should_Return_200()
+        {
+
+            //arrange
+            MockCloudTableWatches mockTable = new MockCloudTableWatches(new Uri("http://127.0.0.1:10002/devstoreaccount1/reports"));
+            Watch watchRequest = TestFactory.GetWatchRequest();
+            DefaultHttpRequest request = TestFactory.CreateHttpRequest(watchRequest);
+
+            //act
+            IActionResult response = await WatchAPI.CreateRegister(request, mockTable, logger);
+
+            //assert
+            OkObjectResult result = (OkObjectResult)response;
+            Assert.Equal(StatusCodes.Status200OK, result.StatusCode);
+        }
+
+
+        [Fact]
+        public async void UpdateWatch_Should_Return_200()
+        {
+
+            //arrange
+            MockCloudTableWatches mockTable = new MockCloudTableWatches(new Uri("http://127.0.0.1:10002/devstoreaccount1/reports"));
+            Watch watchRequest = TestFactory.GetWatchRequest();
+            Guid watchId = Guid.NewGuid();
+            DefaultHttpRequest request = TestFactory.CreateHttpRequest(watchRequest);
+
+            //act
+            IActionResult response = await WatchAPI.UpdateRegisterById(request, mockTable, watchId.ToString(), logger);
+
+            //assert
+            OkObjectResult result = (OkObjectResult)response;
+            Assert.Equal(StatusCodes.Status200OK, result.StatusCode);
+        }
+
+    }
+}
